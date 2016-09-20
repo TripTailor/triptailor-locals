d68652e4ffd8af75859f1b241f353c8bed3176db
Samuel Heaney <sheaney@gmail.com>
2016-09-20 16:58:29 -0500

    Registration endpoints receive json payloads

--- app/controllers/RegistrationController.scala
+++ app/controllers/RegistrationController.scala
@@ -2,60 +2,40 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.data.{Form, Forms}
-import play.api.i18n.{Lang, Messages, MessagesApi}
+import play.api.data.validation.ValidationError
+import play.api.libs.json.{JsError, JsPath}
 import play.api.libs.mailer.MailerClient
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, BodyParsers, Controller}
 import services.EmailService
 
 import scala.concurrent.Future
 
-case class ContactParams(name: String, age: Int, email: String, number: String, text: String)
-
 @Singleton
-class RegistrationController @Inject()(mailer: MailerClient, msgsApi: MessagesApi) extends Controller {
+class RegistrationController @Inject()(mailer: MailerClient) extends Controller {
+  implicit val ec = scala.concurrent.ExecutionContext.global
 
-  def registerNumber = Action.async { implicit request =>
-    numberMapping.bindFromRequest.fold(
-      hasErrors = mapInvalidArguments,
-      success   = registerContact
+  def registerNumber = Action.async(BodyParsers.parse.json) { request =>
+    request.body.validate[NumberForm].fold(
+      invalid = mapInvalidArguments,
+      valid   = registerContact
     )
   }
 
-  def registerEmail = Action.async { implicit request =>
-    emailMapping.bindFromRequest.fold(
-      hasErrors = mapInvalidArguments,
-      success   = registerContact
+  def registerEmail = Action.async(BodyParsers.parse.json) { request =>
+    request.body.validate[EmailForm].fold(
+      invalid = mapInvalidArguments,
+      valid   = registerContact
     )
   }
-
-  private val numberMapping = Form(
-    Forms.mapping(
-      "name"       -> Forms.nonEmptyText,
-      "categories" -> Forms.seq(Forms.of[Categories.Category]),
-      "number"     -> Forms.nonEmptyText
-    )(NumberForm.apply)(NumberForm.unapply)
-  )
-
-  private val emailMapping = Form(
-    Forms.mapping(
-      "name"       -> Forms.nonEmptyText,
-      "categories" -> Forms.seq(Forms.of[Categories.Category]),
-      "email"      -> Forms.email,
-      "questions"  -> Forms.text
-    )(EmailForm.apply)(EmailForm.unapply)
-  )
 
   private def registerContact(params: RegistrationData) = {
-    val ec = scala.concurrent.ExecutionContext.global
-    val service = new EmailService(params, mailer)(ec)
-    service.send().map {
+    new EmailService(params, mailer).send().map {
       case true  => Ok
       case false => ServiceUnavailable
-    }(ec)
+    }
   }
 
-  private def mapInvalidArguments(f: Form[_]) =
-    Future.successful(BadRequest(f.errorsAsJson(Messages(Lang.defaultLang, msgsApi))))
+  private def mapInvalidArguments(errors: Seq[(JsPath, Seq[ValidationError])]) =
+    Future.successful(BadRequest(JsError.toJson(errors)))
 
 }
