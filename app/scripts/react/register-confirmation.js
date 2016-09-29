afbfcf3424c7c6427faf286ac678f4c4d0429cf3
lgaleana <lsgaleana@gmail.com>
2016-09-29 10:28:19 -0500

    Created skeleton for registration confirmation view

---
+++ app/scripts/react/register-confirmation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const RegisterConfirmation = (props) => (
+  <div></div>
+);
+
+
+ReactDOM.render(<RegisterConfirmation />, document.getElementById("content"));
